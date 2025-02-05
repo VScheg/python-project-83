eb7f2267949afa023287179ccdcf9c4d43c0bbbc
Валерия Щеголькова <valeria@Mac-mini.local>
2025-02-05 20:59:02 +0300

    init structure with base templates

---
+++ page_analyzer/app.py
@@ -0,0 +1,25 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    url_for,
+    redirect,
+    flash,
+    get_flashed_messages,
+)
+
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+@app.route('/')
+def index():
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('base/index.html', messages=messages)
