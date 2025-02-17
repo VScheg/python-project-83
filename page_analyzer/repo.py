917c77d8a4e24b5d9a4a77199624ee12c9ac415e
Валерия Щеголькова <broodingthinker@gmail.com>
2025-02-17 12:11:30 +0300

    updated repo

--- page_analyzer/repo.py
+++ page_analyzer/repo.py
@@ -6,88 +6,94 @@ class UrlRepository:
     def __init__(self, db_url):
         self.db_url = db_url
 
-    def get_connection(self):
-        return psycopg2.connect(self.db_url)
+    def execute_query(
+            self,
+            query,
+            params,
+            cursor_factory=None
+    ):
+        with psycopg2.connect(self.db_url) as conn:
+            with conn.cursor(cursor_factory=cursor_factory) as cur:
+                cur.execute(query, params)
+                result = cur.fetchone()
+            conn.commit()
+        return result
 
     def add_url(self, url):
-        with self.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    "INSERT INTO urls (url) VALUES (%s) RETURNING id",
-                    (url,)
-                )
-                id = cur.fetchone()[0]
-
-            conn.commit()
-        return id
+        query = "INSERT INTO urls (url) VALUES (%s) RETURNING id"
+        params = (url,)
+        return self.execute_query(query, params)[0]
 
     def url_info(self, url_id):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT * FROM urls WHERE id = %s", (url_id,))
-                return cur.fetchone()
+        query = "SELECT * FROM urls WHERE id = %s"
+        params = (url_id,)
+        return self.execute_query(
+            query,
+            params,
+            cursor_factory=RealDictCursor
+        )
 
     def get_url_id(self, url):
-        with self.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute("SELECT id FROM urls WHERE url = %s", (url,))
-                return cur.fetchone()[0]
+        query = "SELECT id FROM urls WHERE url = %s"
+        params = (url,)
+        return self.execute_query(query, params)[0]
 
     def get_url(self, url_id):
-        with self.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    "SELECT url FROM urls WHERE id = %s",
-                    (url_id,)
-                )
-                return cur.fetchone()[0]
+        query = "SELECT url FROM urls WHERE id = %s"
+        params = (url_id,)
+        return self.execute_query(query, params)[0]
 
 
 class CheckRepository:
     def __init__(self, db_url):
         self.db_url = db_url
 
-    def get_connection(self):
-        return psycopg2.connect(self.db_url)
+    def execute_query(
+            self,
+            query,
+            params,
+            fetchone=False,
+            flag=True,
+            cursor_factory=None
+    ):
+        with psycopg2.connect(self.db_url) as conn:
+            with conn.cursor(cursor_factory=cursor_factory) as cur:
+                cur.execute(query, params)
+                if flag:
+                    result = cur.fetchone() if fetchone else cur.fetchall()
+                else:
+                    result = None
+            conn.commit()
+        return result
 
     def add_check(self, url_check, url_id):
-        with self.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    """INSERT INTO checks
-                    (status_code, url_id, h1, title, description)
-                    VALUES (%s, %s, %s, %s, %s)""",
-                    (
-                        url_check.get('status_code'),
-                        url_id,
-                        url_check.get('h1'),
-                        url_check.get('title'),
-                        url_check.get('description')
-                    )
-                )
-
-            conn.commit()
-        return None
+        query = """INSERT INTO checks
+        (status_code, url_id, h1, title, description)
+        VALUES (%s, %s, %s, %s, %s)"""
+        params = (
+            url_check.get('status_code'),
+            url_id,
+            url_check.get('h1'),
+            url_check.get('title'),
+            url_check.get('description')
+        )
+        return self.execute_query(query, params, flag=False)
 
     def show_checks(self, url_id):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT * FROM checks WHERE url_id = %s", (url_id,))
-                return cur.fetchall()
+        query = "SELECT * FROM checks WHERE url_id = %s"
+        params = (url_id,)
+        return self.execute_query(query, params, cursor_factory=RealDictCursor)
 
     def show_urls(self):
-        with self.get_connection() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute(
-                    """SELECT 
-                    urls.id AS id, 
-                    urls.url AS url, 
-                    checks.created_at AS last_check, 
-                    checks.status_code AS status_code 
-                    FROM urls LEFT JOIN checks ON urls.id = checks.url_id
-                    WHERE checks.id IS NULL OR checks.id = (
-                    SELECT MAX(id) FROM checks WHERE url_id = urls.id
-                    )
-                    ORDER BY id DESC"""
+        query = """SELECT 
+                urls.id AS id, 
+                urls.url AS url, 
+                checks.created_at AS last_check, 
+                checks.status_code AS status_code 
+                FROM urls LEFT JOIN checks ON urls.id = checks.url_id
+                WHERE checks.id IS NULL OR checks.id = (
+                SELECT MAX(id) FROM checks WHERE url_id = urls.id
                 )
-                return cur.fetchall()
+                ORDER BY id DESC"""
+        params = None
+        return self.execute_query(query, params, cursor_factory=RealDictCursor)
