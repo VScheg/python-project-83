9c717ed04a7cacabd46d44e37c9c40de4d3d9362
Валерия Щеголькова <broodingthinker@gmail.com>
2025-02-19 18:09:24 +0300

    updated validator.py

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -2,10 +2,10 @@ from validators.url import url as validate
 from urllib.parse import urlparse
 
 
-def validate_url(url):
-    return True if validate(url) and len(url) < 256 else False
+def validate_url(url: str) -> bool:
+    return True if url and validate(url) and len(url) < 256 else False
 
 
-def normalize_url(url):
+def normalize_url(url: str) -> str:
     parsed = urlparse(url)
     return f'{parsed.scheme}://{parsed.netloc}'.lower()
