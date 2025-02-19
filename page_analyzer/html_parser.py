c8dc06e597c97d213d7c3437cfe3d420e044b437
Валерия Щеголькова <broodingthinker@gmail.com>
2025-02-19 18:10:33 +0300

    updated html_parser.py

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -2,28 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parse_html(url: str) -> dict:
+def get_seo(url: str) -> dict[str, str | int]:
     """
-    Get from HTML page SEO parameters - h1, title, description and status code.
+    Gets from url HTML page and returns SEO parameters - h1, title, description and status code of page.
     """
     response = requests.get(url)
     response.raise_for_status()
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    h1 = soup.find('h1').text if soup.find('h1') else ''
-    title = soup.title.string if soup.title else ''
-    description = soup.find(
-        'meta',
-        attrs={'name': 'description'}
-    )['content'] if soup.find(
-        'meta',
-        attrs={'name': 'description'}
-    ) else ''
-
-    result = {
+    h1, title, description = parse_html(response)
+    return {
         'status_code': response.status_code,
         'h1': h1,
         'title': title,
         'description': description,
     }
-    return result
+
+
+def parse_html(response: requests.models.Response) -> tuple[str, str, str]:
+    """
+    Returns from HTML page SEO parameters - h1, title, description.
+    """
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    h1 = soup.find('h1').text if soup.find('h1') else ''
+    title = soup.title.string if soup.title else ''
+
+    description = find_description['content'] if (
+        find_description := soup.find(
+            'meta',
+            attrs={'name': 'description'}
+        )
+    ) else ''
+
+    return h1, title, description
